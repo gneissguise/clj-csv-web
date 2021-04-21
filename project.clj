ea400ab3b23606529a64099837a45f4ae24f5967
Justin Frost <frostjust@gmail.com>
2021-04-20 17:14:58 -0700

    version bump v.0.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gr-homework "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject gr-homework "0.2.0"
+  :description "CSF File Parsing"
+  :url "https://github.com/gneissguise/gr-homework/blob/data-file-parser/README.md"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
