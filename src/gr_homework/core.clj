64579878434e5366e017d79a921272067c9148e3
Justin Frost <frostjust@gmail.com>
2021-04-21 23:57:15 -0700

    basic cli setup taken from tools.cli github examples

--- src/gr_homework/core.clj
+++ src/gr_homework/core.clj
@@ -1,14 +1,78 @@
 (ns gr-homework.core
+  (:require [clojure.tools.cli :as cli]
+            [clojure.string :as s])
   (:gen-class))
 
-;; TODO: Stage 1
-;; - Add clojure.tools for cli arg handling
-;; - fns to detect delimiter, or provide an arg to set
-;; - generate 3 files for examples
-;; - output 3 ways: sort by gender, birth date ascending, last name descending
+(def sort-keymap {"lastname" :Lastname
+             "gender" :Gender
+             "dateofbirth" :DateOfBirth
+             "demo" :Demo})
 
+(def cli-options
+  [["-s"  "--sort SORT" "Sort option"
+    :default :Demo
+    :parse-fn #(get sort-keymap (s/lower-case %))
+    :validate [#(not
+                 (contains? sort-keymap (s/lower-case %)))
+               "Valid sort options are 'LastName', 'Gender', 'DateOfBirth' or 'Demo' (runs all three on all three files)"]]
+   ["-f" "--file FILE" "Files to load"
+    :multi true
+    :default ["./examples/comma-delim.txt"
+              "./examples/pipe-delim.txt"
+              "./examples/space-delim.txt"]
+    :update-fn conj]
+   ["-h" "--help" "Help"]])
+
+(defn usage [options-summary]
+  (->> ["gr-homework: This is my submission for a cli delimited file parser & REST api for a job interview."
+        ""
+        "Usage: gr-homework [options]"
+        ""
+        "Options:"
+        options-summary
+        ""]
+       (s/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while processing your request:\n\n"
+       (s/join \newline errors)))
+
+
+(defn validate-args
+  "Validate command line arguments. Either return a map indicating the program
+  should exit (with a error message, and optional ok status), or a map
+  indicating the action the program should take and the options provided."
+  [args]
+  (let [{:keys [options errors summary]} (cli/parse-opts args cli-options)]
+    (cond
+      (:help options) ; help => exit OK with usage summary
+      {:exit-message (usage summary) :ok? true}
+      errors ; errors => exit with description of errors
+      {:exit-message (error-msg errors)}
+      options
+      {:options options}
+      :else ; failed custom validation => exit with usage summary
+      {:exit-message (usage summary)})))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn start-up
+  [options]
+  (let [sort (:sort options)
+        files (:file options)
+        head-file (first files)
+        rest-files (rest files)]
+    (println "Sort: " sort)
+    (println "Files: " files)
+    (println "head-file: " head-file)
+    (println "rest-files: " rest-files)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+    (let [{:keys [options exit-message ok?]} (validate-args args)]
+      (if exit-message
+        (exit (if ok? 0 1) exit-message)
+        (start-up options))))
